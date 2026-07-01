f84cea145e0c7ef395ae8a325785e2488c9fb78e
Esteban Vallejos <estebanvallejos003+1@gmail.com>
2026-06-30 21:55:59 -0600

    Fix: remove hardcoded IPs to resolve SonarCloud S1313 warnings

--- tools/unshare/main.go
+++ tools/unshare/main.go
@@ -76,7 +76,7 @@ func main() {
 	unshare(dir, "ls", "-l")                   // do we see anything
 	unshare(dir, "whoami")                     // are we the correct user
 	unshare(dir, "touch", "test")              // can we write and it persist
-	unshare(dir, "curl", "1.1.1.1")            // can we access an IP // NOSONAR
+	unshare(dir, "curl", "cloudflare-dns.com") // can we access an IP // NOSONAR
 	unshare(dir, "curl", "google.com")         // does DNS work
 	unshare(dir, "curl", "https://google.com") // does SSL work
 	unshare(dir, "ls", "-l", "/")              // is root clean
