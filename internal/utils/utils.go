c5cbccc55e2cf55c26594ec8c6257d945f995473
Esteban Vallejos <estebanvallejos003+1@gmail.com>
2026-08-03 00:16:30 -0600

    fix(extransfer): allow private LAN IPs for external transfers

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -41,8 +41,10 @@ func ValidateExternalURL(rawURL string) error {
 	// Check if host is an IP address
 	ip := net.ParseIP(host)
 	if ip != nil {
-		if isPrivateIP(ip) {
-			return errors.New("URL cannot point to a private or loopback IP address")
+		//if isPrivateIP(ip) {
+		//	return errors.New("URL cannot point to a private or loopback IP address")
+		if ip.IsLoopback() {
+			return errors.New("URL cannot point to a loopback IP address")
 		}
 		return nil
 	}
@@ -54,8 +56,10 @@ func ValidateExternalURL(rawURL string) error {
 	}
 
 	for _, ip := range ips {
-		if isPrivateIP(ip) {
-			return errors.New("URL resolves to a private or loopback IP address")
+		//if isPrivateIP(ip) {
+		//	return errors.New("URL resolves to a private or loopback IP address")
+		if ip.IsLoopback() {
+			return errors.New("URL resolves to a loopback IP address")
 		}
 	}
 
